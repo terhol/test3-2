305f83b46e8b2c99256d6e648664e9d9eeeb8150
Tereza Holm <holm.tereza@gmail.com>
2019-01-17 13:18:39 +0100

    Project finished.

--- src/se/terhol/pisemka32/VendorImpl.java
+++ src/se/terhol/pisemka32/VendorImpl.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.TreeSet;
@@ -14,15 +16,15 @@ import java.util.TreeSet;
  * @author Tereza Holm
  */
 public class VendorImpl implements Vendor {
-    private SortedMap<Magazine, Double> myMagazines = new TreeMap<>();
+    private SortedMap<Magazine, Double> magazine = new TreeMap<>();
 
     @Override
     public double getPrice(Magazine magazine) {
         double price;
-        if (magazine == null || !myMagazines.containsKey(magazine)) {
+        if (magazine == null || !this.magazine.containsKey(magazine)) {
             price = -1;
         } else {
-            price = myMagazines.get(magazine);
+            price = this.magazine.get(magazine);
         }
         return price;
     }
@@ -32,27 +34,20 @@ public class VendorImpl implements Vendor {
         if (price < 0 || magazine == null) {
             throw new IllegalArgumentException();
         }
-        double originalPrice;
-        myMagazines.put(magazine, price);
-        if (!myMagazines.containsKey(magazine)) {
-            originalPrice = -1;
-        } else {
-            originalPrice = myMagazines.get(magazine);
+        double originalPrice = -1;
+        if (this.magazine.containsKey(magazine)) {
+            originalPrice = this.magazine.get(magazine);
         }
+        this.magazine.put(magazine, price);
         return originalPrice;
     }
 
     @Override
     public Collection<Magazine> getMostExpensive() {
-        Collection<Magazine> mostExpensive = new TreeSet<>();
-        double highestPrice = 0;
-        for (Double magazinePrice : myMagazines.values()) {
-            if (magazinePrice > highestPrice) {
-                highestPrice = magazinePrice;
-            }
-        }
-        for (Magazine magazine : myMagazines.keySet()) {
-            if (myMagazines.get(magazine) == highestPrice) {
+        Set<Magazine> mostExpensive = new TreeSet<>();
+        double highestPrice = Collections.max(magazine.values());
+        for (Magazine magazine : magazine.keySet()) {
+            if (this.magazine.get(magazine) == highestPrice) {
                 mostExpensive.add(magazine);
             }
         }
@@ -63,7 +58,7 @@ public class VendorImpl implements Vendor {
     public void save(OutputStream os) throws IOException {
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os));
 
-        for (Magazine magazine : myMagazines.keySet()) {
+        for (Magazine magazine : magazine.keySet()) {
             writer.write(magazine.getName() + " /" + magazine.getIssue() + "/: " + this.getPrice(magazine));
             writer.newLine();
             writer.flush();
